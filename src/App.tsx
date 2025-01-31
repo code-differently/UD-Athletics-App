6cdc7c6a71af7dede6016cc63b4b7579c129cd01
Mya <86239804+AmiyahJo@users.noreply.github.com>
2025-01-31 10:35:21 -0500

    Feat: adds starter-layout (#2)
    * feat: add core react libraries & typescript definitions for react
    
    Installed react
    
    * feat: adds placeholder header
    
    * feat: adds empty index file
    
    details to add later
    
    * feat: add  .gitignore file
    
    * rm: node_modules directory
    
    * feat: Incorporate NextJs
    
    using documentation to include recommended files + directories
    
    * feat: adds Vitest
    
    Installed for unit testing
    
    * feat: adds cypress
    
    installed for integration testing
    
    * feat: adds more .gitignore details
    
    * feat: adds cypress folder structure
    
    * feat: adds vitest.config file
    
    ---------
    
    Co-authored-by: AmiyahJo <ajones244@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+    return(
+        <div>
+            <h1>Starter Layout</h1>
+            <p>Placeholder components here!</p>
+        </div>
+    )
+}
