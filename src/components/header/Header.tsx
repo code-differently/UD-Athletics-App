5b821298a251ec2a490adfe6509438e5ada4d641
AmiyahJo <ajones244@gmail.com>
2025-02-03 11:36:44 -0500

    feat: adds Header html
    image is not yet included!

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Header = () => {
-    return <header><h2>Header Placeholder</h2></header>
-}
+    return (
+        <header className="header">
+            <div className="header-logo">
+                <a href="/">
+                {/* <img src={logoImg} alt="UD Logo" /> */}
+                </a>
+            </div>
+            <ul className="header-top-menu">
+                <li>
+                  <a href="#">Home</a>  
+                </li>
+                <div className="header-cta">
+                    <a className="team-button" href="#">
+                        Team
+                    </a>
+                </div>
+            </ul>
+        </header>
+    );
+};
 
 export default Header; 
