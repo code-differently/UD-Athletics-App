cf2dc54d6f4da75730045387f97f9e8d37e9c980
AmiyahJo <ajones244@gmail.com>
2025-02-03 11:41:06 -0500

    feat: adds header to page.tsx
    to include in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import  AvatarScene  from "../components/avatar/AvatarScene";
+import Header from "../components/header/Header"
 
 export default function Page() {
   return (
     <main>
+      <Header/>
       <h1>Welcome to My App</h1>
       <AvatarScene />  
     </main>
