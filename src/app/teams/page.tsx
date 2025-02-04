929017099d8b9991d05767b36a7084f06e50eec1
AmiyahJo <ajones244@gmail.com>
2025-02-04 09:49:54 -0500

    Feat: adds teams page

---
+++ src/app/teams/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TeamsPage() {
+    return (
+        <main className="">
+            <h1>Teams page</h1>
+            <p>Welcome to the teams section!</p>
+
+        </main>
+    );
+}
