3f5d6c6290331284c4bd70d94d164d93f0e3b1e0
AmiyahJo <ajones244@gmail.com>
2025-02-07 09:42:29 -0500

    feat: new avatar scene
    made to simply make the avatar itself with no customizer

---
+++ src/components/avatar/AvatarScene.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
+
+
+
+export default AvatarScene
