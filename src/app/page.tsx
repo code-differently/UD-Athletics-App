7d81167177cc88dc85dd38e8d571426970526297
AmiyahJo <ajones244@gmail.com>
2025-02-13 14:26:20 -0500

    feat: adds help button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/header/Header"
 import AvatarScene from "../components/avatar/AvatarScene";
+import HelpButton from "../components/HelpButton";
 
 export default function Page() {
   return (
     <main className="min-h-screen">
       <Header/>
       <h1>Welcome to My App</h1>
+      <HelpButton/>
       <AvatarScene />  
     </main>
   );
