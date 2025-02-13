eff9b93e3cf515a608837bcf35738cf11db11fcd
Mya <86239804+AmiyahJo@users.noreply.github.com>
2025-02-13 11:12:06 -0500

    feat: adds clickable 3d body part  (#10)
    * Update package.json
    
    * Update package-lock.json
    
    * Update package.json
    
    * chore: downgrade react vers
    
    * rm: avatarCntainer
    
    * feat: adds Avatar scene
    
    replaces AvatarCustomizer , removing the color option + adding the clickable body part
    
    * rm: comments in AvatarScene.tsx
    
    * feat: adds avatarScene test file
    
    * feat: adds integration folder with e2e tests
    
    includes loading the component , clicking on the avatar, and verifying the clickable zone
    
    * Delete src/__tests__/AvatarScene.test.tsx
    
    ---------
    
    Co-authored-by: AmiyahJo <ajones244@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Header from "../components/header/Header"
-import AvatarCustomizer from "../components/avatar/AvatarCustomizer";
+import AvatarScene from "../components/avatar/AvatarScene";
 
 export default function Page() {
   return (
     <main className="min-h-screen">
       <Header/>
       <h1>Welcome to My App</h1>
-      <AvatarCustomizer />  
+      <AvatarScene />  
     </main>
   );
   }
