d5b7b65914b6c010c9b9ff48f1f7473601db5a8d
AmiyahJo <ajones244@gmail.com>
2025-02-18 11:35:33 -0500

    feat: adds AvatarContainer
    to hold the avatar and render the clickable body parts ontop

---
+++ src/components/avatar/AvatarContainer.tsx
@@ -0,0 +1,18 @@
+import AvatarScene from "./AvatarScene";
+import AvatarWithBodyPart from "./AvatarWithBodyPart";
+
+const AvatarContainer: React.FC = () => {
+  return (
+    <div className="relative w-[300px] h-[300px]">
+      {/* Avatar Scene (3D Model) */}
+      <AvatarScene />
+
+      {/* Clickable Body Parts Overlay */}
+      <div className="absolute top-0 left-0 w-full h-full">
+        <AvatarWithBodyPart />
+      </div>
+    </div>
+  );
+};
+
+export default AvatarContainer;
