f385fef795dfee85c1634937bf65fdf9e8978395
AmiyahJo <ajones244@gmail.com>
2025-02-18 11:57:23 -0500

    feat: replace the avatarScene component with avatarContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Header from "../components/header/Header"
-import AvatarScene from "../components/avatar/AvatarScene";
+import AvatarContainer from "../components/avatar/AvatarContainer";
 
 export default function Page() {
   return (
     <main className="min-h-screen">
       <Header/>
       <h1>Welcome to My App</h1>
-      <AvatarScene />  
+      <AvatarContainer />  
     </main>
   );
   }
