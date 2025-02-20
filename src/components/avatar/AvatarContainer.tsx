79e56f0e01fce321ebd97ac3da8548084870ce75
AmiyahJo <ajones244@gmail.com>
2025-02-20 09:45:37 -0500

    feat: adds bodyPartmarkers import
    to overlay ontop of the avatar

--- src/components/avatar/AvatarContainer.tsx
+++ src/components/avatar/AvatarContainer.tsx
@@ -1,11 +1,15 @@
 import AvatarScene from "./AvatarScene";
+import BodyPartMarkers from "./BodyPartMarkers";
 
 const AvatarContainer: React.FC = () => {
   return (
     <div className="relative w-[300px] h-[300px]">
       {/* Avatar Scene (3D Model) */}
       <AvatarScene />
+      {/* Body Part Markers overlay*/}
+      <BodyPartMarkers />
     </div>
+
   );
 };
 
